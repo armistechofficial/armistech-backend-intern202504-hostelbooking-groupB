01d9559c46cd9d4b07618d94da15dc2b689d924b
Erika-Shrestha <erikashrestha333@gmail.com>
2025-06-08 01:33:27 +0545

    The mongodb compass and node.js is successfully connected

--- database/mongodb.js
+++ database/mongodb.js
@@ -4,3 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const mongoURL = "mongodb://localhost:27017/Hostel-Booking"
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(mongoURL);
+        console.log("The Mongodb database is connected successfully.");
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+export {connectDB};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import {connectDB} from "./database/mongodb.js";
+
 const app = express();
 const port = 7000;
 //app.get(para1, para2, para3) where first is blank path, second and third is the function run in the path i.e. 
@@ -8,6 +10,7 @@ app.get('/', (req, res) => {
   })
 
 //listens to the port 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+app.listen(port, async () => {
+    console.log(`App listening on port ${port}`);
+    await connectDB();
   })
