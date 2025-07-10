300a048b4e69a077bda4147895fc1da8b2d1d750
Kritika <kritikapanthi45@gmail.com>
2025-07-10 19:38:21 +0545

    Update emailRoutes.js

--- footer_email/routes/emailRoutes.js
+++ footer_email/routes/emailRoutes.js
@@ -1,9 +1,13 @@
-console.log("Email routes loaded");
+console.log("Email routes loaded"); // Log to confirm that the email routes file has loaded
 
 const express = require("express");
 const router = express.Router();
+
+// Import the subscribeEmail controller function
 const { subscribeEmail } = require("../controllers/emailController");
 
+// Route to handle email subscription from footer
 router.post("/subscribe", subscribeEmail);
 
+// Export the router to be used in the main app
 module.exports = router;
