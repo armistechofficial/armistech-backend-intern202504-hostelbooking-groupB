b1bc13e03e4d59dd383e01072bd7851cc32a1715
Kritika <kritikapanthi45@gmail.com>
2025-07-10 19:47:14 +0545

    Implement review controller with validation

--- client_reviews/controllers/reviewController.js
+++ client_reviews/controllers/reviewController.js
@@ -1,20 +1,25 @@
 const Review = require('../models/Review');
 const reviewSchema = require('../validations/reviewValidation');
 
+// Controller to add a new review
 exports.addReview = async (req, res) => {
   try {
     const validated = reviewSchema.parse(req.body);
+    //Create a new Review document and save to the database
     const newReview = new Review(validated);
     await newReview.save();
+    
     res.status(201).json({ message: "Review submitted successfully", review: newReview });
   } catch (error) {
-    if (error.name === 'ZodError') {
+    // Handle Zod validation errors
+    if (error.name === 'ZodError') { 
       return res.status(400).json({ error: error.errors });
     }
     res.status(500).json({ error: "Server error" });
   }
 };
 
+//  Controller to fetch all reviews
 exports.getAllReviews = async (req, res) => {
   try {
     const reviews = await Review.find().sort({ createdAt: -1 });
