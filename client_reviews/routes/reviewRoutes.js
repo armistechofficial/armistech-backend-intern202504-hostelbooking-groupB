0287bd2250569bd35172dd38d1b0af1e8d1147c0
Erika-Shrestha <erikashrestha333@gmail.com>
2025-07-12 01:59:33 +0545

    Remove all files except README.md

--- client_reviews/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reviewController = require('../controllers/reviewController');
-
-router.post('/add', reviewController.addReview);
-
-// GET - get all reviews
-router.get('/', reviewController.getAllReviews);
-module.exports = router;
